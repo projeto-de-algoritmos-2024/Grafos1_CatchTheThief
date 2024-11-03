ca6746997d7ac05bfcbada7c8d2249798b68ab82
karol <karol.cavalcanti37@gmail.com>
2024-11-03 10:37:49 -0300

    adiciona função que pede número de suspeitos
    Co-authored-by: Guilherme de Sá Gonçalves <GuilhermeDSa1013@users.noreply.github.com>

--- CatchTheThief.py
+++ CatchTheThief.py
@@ -2,7 +2,7 @@ import time
 import os
 
 # Função que diminui a velocidade q o texto é escrito no terminal
-def diminui_velocidade_texto(texto, PouI, delay=0.05):
+def diminui_velocidade_texto(texto, PouI, delay=0.001):
     for char in texto:
         print(char, end='', flush=True)
         time.sleep(delay)
@@ -11,17 +11,31 @@ def diminui_velocidade_texto(texto, PouI, delay=0.05):
         return resposta
     else:
         print()
+        
+# Função que limita valor de N
+def pede_N():
+    while True:
+        try:
+            N = int(input("\033[3m(digite um valor entre 3 e 15):\033[0m "))
+            if 3 <= N <= 15:
+                return N
+            else:
+                print("\033[3m(número fora do intervalo, tente novamente)\033[0m")
+        except ValueError:
+            print("\033[3m(digite um número inteiro)\033[0m")
+
+
 
 # Limpa terminal antes de rodar
 os.system("clear")
 
-diminui_velocidade_texto("Johan Towns (JT): Shells! Shells! Estão pedindo nossa ajuda em um caso...\nParece que na noite passada, uma obra de arte muito valiosa do museu Hermitage, aqui em São Petersburgo, foi roubada.\nE o culpado ainda não foi encontrado, porém com ajuda das autoridades locais, alguns suspeitos foram detidos.\nAgora querem nossa ajuda, o infame Detetive Shells Shock, e seu fiel escudeiro Sr. Johan Towns!\nApenas nós conseguiríamos decifrar esse misté-\n\nShells Shock (SS): Está atrasado, caro Sr. Towns, já falei com as autoridades, e esse caso é mais simples do que parece, já tenho um breve ideia do quem é o culpado, apenas com o que foi falado.", "P")
+# Contexto da história
+diminui_velocidade_texto("Johan Towns (JT): Shells! Shells! Estão pedindo nossa ajuda em um caso...\nParece que na noite passada, uma obra de arte muito valiosa do museu Hermitage, aqui em São Petersburgo, foi roubada.\nE o culpado ainda não foi encontrado, porém com ajuda das autoridades locais, alguns suspeitos foram detidos.\nAgora querem nossa ajuda, o infame Detetive Shells Shock, e seu fiel escudeiro Sr. Johan Towns!\nApenas nós conseguiríamos decifrar esse misté-\n\nShells Shock (SS): Está atrasado, caro Sr. Towns, já falei com as autoridades, e esse caso é mais simples do que parece,\njá tenho um breve ideia de quem é o culpado, apenas com o que foi dito.", "P")
 
 # N é o número de suspeitos, nossos vértices
-N = diminui_velocidade_texto("\nJT: Estou chocado com sua astúcia Detetive Shock. Então, para começar, quantos suspeitos são exatamente? ", "I")
-diminui_velocidade_texto(f"SS: São exatamente {N} supeitos.", "P")
+diminui_velocidade_texto("\nJT: Estou chocado com sua astúcia Detetive Shock. Então, para começar, quantos suspeitos são exatamente?: ", "P")
 
-
+diminui_velocidade_texto(f"SS: São exatamente {pede_N()} supeitos.", "P")
 
 diminui_velocidade_texto(f"\nN: Então os suspeitos são: Por gentileza Detetive, poderia nos dizer o que cada suspeito está afirmando?", "P")
 
